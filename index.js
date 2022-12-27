8a31df4b745d39d8512b620ef33bffa780397861
Pervez <pervezhossain1998@gmail.com>
2022-12-27 12:18:14 +0600

    Database Concetion Stablished

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
 // MiddleWare
 app.use(cors())
 app.use(express.json())
 require("dotenv").config()
+
+// Database Setup
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xu0oole.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        
+    }
+    finally {
+        
+    }
+}
+run().catch(error=> console.error(error))
+
+
 
 app.get("/", (req, res) => {
     res.send("Server is Running")
